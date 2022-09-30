d251a066b53ce18a99e16e869bb74650289e8661
imiranda <igor.miranda@foursys.com.br>
2022-09-30 11:31:49 -0300

    configurando a model para o room

--- app/src/main/java/com/example/dogsapp/view/view/model/DogBreed.java
+++ app/src/main/java/com/example/dogsapp/view/view/model/DogBreed.java
@@ -1,28 +1,40 @@
 package com.example.dogsapp.view.view.model;
 
+import androidx.room.ColumnInfo;
+import androidx.room.PrimaryKey;
+
 import com.google.gson.annotations.SerializedName;
 
+//@ColumnInfo é parte do ROOM
+
 public class DogBreed {
+    @ColumnInfo(name = "breed_id")
     @SerializedName("id")
     public String breedId;
 
+    @ColumnInfo(name = "name")
     @SerializedName("name")
     public String dogBreed;
 
+    @ColumnInfo(name = "life_span")
     @SerializedName("life_span")
     public String lifeSpan;
 
+    @ColumnInfo(name = "bredd_group")
     @SerializedName("breed_group")
     public String breedGroup;
 
+    @ColumnInfo(name = "breed_for")
     @SerializedName("bred_for")
     public String bredFor;
 
     public String temperament;
 
+    @ColumnInfo(name = "dog_url")
     @SerializedName("url")
     public String imageUrl;
 
+    @PrimaryKey(autoGenerate = true)
     public int uui;
 
     public DogBreed(String breedId, String dogBredd, String lifeSpan, String breedGroup, String bredFor, String temperament, String imageUrl) {
